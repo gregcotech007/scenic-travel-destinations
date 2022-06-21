944e33630ca11b7e16717761f0a040a8e0a2c4d7
gregcotech007 <commins.greg@outlook.com>
2022-06-21 12:15:15 +0000

    Deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://fwjboawzdigbqq:420a47afd9fa46a6238c9d722ba5afdeb3ce47c8513b46647c01ae3ceb901315@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d1hli4idfmgn9c"
+os.environ["SECRET_KEY"] = "IloveicecreamintheSummertime99!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://442218363159532:hkbMmnH0u5AMhL4S6XbOUIW7YYU@gregcotech007"
